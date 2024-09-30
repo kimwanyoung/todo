27ac7358e886fd9d0a14da7b37b5d34789cf77bc
kimwanyoung <dhks2869@gmail.com>
2024-09-30 16:27:10 +0900

    fix : schema.sql 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,18 +5,18 @@ USE TODO;
 
 CREATE TABLE IF NOT EXISTS USER
 (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    user_id  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(100)    NOT NULL,
+    password VARCHAR(100)    NOT NULL,
+    email    VARCHAR(100)    NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS TODO
 (
-    todo_id INT PRIMARY KEY AUTO_INCREMENT,
-    contents VARCHAR(100) NOT NULL,
-    created_at DATE NOT NULL,
-    updated_at DATE NOT NULL,
-    user_id INT,
-    FOREIGN KEY(user_id) REFERENCES USER(user_id) ON DELETE CASCADE
+    todo_id    INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    contents   VARCHAR(100)    NOT NULL,
+    created_at DATETIME        NOT NULL default now(),
+    updated_at DATETIME        NOT NULL default now() on update now(),
+    user_id    INT,
+    FOREIGN KEY (user_id) REFERENCES USER (user_id) ON DELETE CASCADE
 );
